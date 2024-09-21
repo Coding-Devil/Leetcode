aba08c409e477d0dc98ca34fdb68ae6523f26afc
Gokulnath V ✨ <93648787+Coding-Devil@users.noreply.github.com>
2024-09-22 00:14:48 +0530

    Time: 53 ms (59.03%) | Memory: 12.5 MB (74.51%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[nums[i]] = i
+
+        return []  # No solution found
